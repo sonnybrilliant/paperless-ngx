3aaa14a5f74c83b571599b68a2b2c5e66e0b45d9
sonnybrilliant <ronald.conco@mlankatech.co.za>
2022-11-21 04:26:18 +0200

    updated how config file read

--- src/paperless/settings.py
+++ src/paperless/settings.py
@@ -22,6 +22,7 @@ elif os.path.exists("/etc/paperless.conf"):
 elif os.path.exists("/usr/local/etc/paperless.conf"):
     load_dotenv("/usr/local/etc/paperless.conf")
 elif os.path.exists("/home/ubuntu/source/paperless-ngx/paperless.conf"):
+    load_dotenv("/home/ubuntu/source/paperless-ngx/paperless.conf")
 
 # There are multiple levels of concurrency in paperless:
 #  - Multiple consumers may be run in parallel.
